3cea7a43d5759ee715b24bbaa25cc7c34cd600f7
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-27 08:49:34 -0300

    mismo error que el primer tp que no se reconoce .sql

---
+++ DB/index.js
@@ -0,0 +1,29 @@
+import { Express } from "express";
+const app = express();
+import { PF_ArgTeam_Services } from "./servicies/DB-servicies.js";
+const port = 3000;
+
+app.use(express.json())
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
+
+app.post('/logInUsuario', async (req, res) => {
+    console.log("en post, req:", req)
+    let username = req.body.username;
+	let password = req.body.password;
+    try {
+        console.log(req);
+        const results = await PF_ArgTeam_Services.logInFunction(username, password);
+        if(results != undefined) {
+            res.status(200).json({ message: 'Usuario Verificado' });
+        } else {
+            res.status(500).json({ error: 'Nombre de usuario y/o contraseña incorrecta' });
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Nombre de usuario y/o contraseña incorrecta' });
+    }
+    console.log(username, password);
+})
---
+++ DB/models/Usuario.js
@@ -0,0 +1,8 @@
+class Usuario {
+    id;
+    username;
+    password;
+    mail;
+}
+
+export default Usuario;
